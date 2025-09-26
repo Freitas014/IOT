82f8d24df4bcd6b3a7337fe989024d5e2ff6e306
Luís Fernando de Freitas <freitasluisito508@gmail.com>
2025-09-26 17:10:55 -0300

    Create for_contador.c

---
+++ Exemplos/for/for_contador.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+    int i, num. cont;
+    cont = 0;
+    //inicialização da variável CONT com o valor zero
+    for (i = 1; i <= 5; i++)
+    {
+        printf("Digite um número: ");
+        scanf("%d%*c", &num);
+        if (num >5)
+        cont = cont +1; //contando mais na variável CONT
+    }
+        printf("Quantide de numeros maiores que 5 = %d", cont);
+}
