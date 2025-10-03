e3fc15c1a138ad6ea5476997f41835156bc5b771
Luís Fernando de Freitas <freitasluisito508@gmail.com>
2025-10-03 17:12:37 -0300

    Create vet_matriz.cpp

---
+++ ATV/ATV08 (matriz)/vet_matriz.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	int vet["Lojas A"];
+	int matriz[3][6];
+	int i, j;
+	
+	for (j=0; j<18; j++){
+		printf("|%d|", vet[i]);
+	
+	for (i=0; i<3; i++)
+	{
+		printf("\nElementos da linha %d ", i);
+	for (j=0; j<6; j++){
+		printf("|%d|", matriz[]);
+	}
+	}
+	}
+}
