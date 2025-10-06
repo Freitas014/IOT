88c5e6d26369ffe578cce9a6531342c67cdaaff9
Luís Fernando de Freitas <freitasluisito508@gmail.com>
2025-10-06 17:09:05 -0300

    Create a.cpp

---
+++ Exemplos/biblioteca/a.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <locale.h>
+#include "C:\Users\luis_f_de-freitas\Documents\GitHub\IOT\Exemplos\biblioteca\positivo&negativo.h"
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	int num1, num2;
+	sub_rotina4();
+	validaNum();
+	printf("Digite o primeiro numero: ");
+	scanf("%d", &num1)
+	printf("Digite o segundo numero: ");
+	scanf("%d", &num2)
+}
+	
+	
