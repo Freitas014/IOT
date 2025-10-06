259e3b887185eaadcb8323681208881e832ac1d2
Luís Fernando de Freitas <freitasluisito508@gmail.com>
2025-10-06 15:00:01 -0300

    Update vet_matriz.cpp

--- ATV/ATV08 (matriz)/vet_matriz.cpp
+++ ATV/ATV08 (matriz)/vet_matriz.cpp
@@ -4,19 +4,26 @@
 
 int main(){
 	setlocale(LC_ALL, "Portuguese");
-	int vet["Lojas A"];
+	int vet[6];
 	int matriz[3][6];
 	int i, j;
 	
-	for (j=0; j<18; j++){
-		printf("|%d|", vet[i]);
+	printf("     colunas:        ");
+	for (i = 0; i < 6; i++){
+		vet[i] = i + 1;
+		printf("%d|", vet[i]);
+	}
+		
+	for (i = 0; i<3; i++){
+	printf("\nElementos da linha %d ", i);                     
 	
-	for (i=0; i<3; i++)
-	{
-		printf("\nElementos da linha %d ", i);
 	for (j=0; j<6; j++){
-		printf("|%d|", matriz[]);
+		matriz[i][j] = i + j;
+		printf("%d|", matriz[i][j]);
 	}
-	}
-	}
-}
+}}
+
+		
+	                   
+
+	
