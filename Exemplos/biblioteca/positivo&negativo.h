87d83df973893d2942bf73e2347fa60010e995b1
Luís Fernando de Freitas <freitasluisito508@gmail.com>
2025-10-06 17:09:07 -0300

    Create positivo&negativo.h

---
+++ Exemplos/biblioteca/positivo&negativo.h
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+void sub_rotina4(){
+	int num1, num2;
+	printf("\nDigite um numero: \n");
+	scanf("%d%*c", &num1);
+	printf("\nDigite outro numero: \n");
+	scanf("%d%*c", &num2);
+	}
+void validaNum(){
+
+	if (a > 0){
+		printf("Numero Positivo");
+	}
+	else{
+		printf("Numero Negativo");
+	}
+	if (b > 0){
+		printf("Numero Positivo");
+	}
+	else{
+		printf("Numero Negativo");
+	}
+	}
